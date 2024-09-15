5db7cfb4ffda569746c592bf8b10fd8038f023cc
vikernes1981 <kostaspapas321@gmail.com>
2024-09-15 18:21:45 +0200

    Notes Summary with AI : DONE ( I hope )

--- fe-notes-app/src/components/Diary/MoodAIAnalysis.jsx
+++ fe-notes-app/src/components/Diary/MoodAIAnalysis.jsx
@@ -1,16 +1,14 @@
 import { useRef, useState } from 'react';
-import { Bar } from 'react-chartjs-2';
 import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js';
 import Charts from './Charts'; // Import Charts component
 
 ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale);
+
 const apiKey = import.meta.env.VITE_GEN_AI_API_KEY;
 const MoodAIAnalysis = ({ entries }) => {
   const modalRef = useRef();
   const [loading, setLoading] = useState(false);
   const [aiSummary, setAiSummary] = useState(null); // To store AI result
-
-  console.log('Entries:', entries); // Log the entries values
 
   const handleAISummary = async () => {
     setLoading(true); // Start loading state
@@ -28,12 +26,12 @@ const MoodAIAnalysis = ({ entries }) => {
         stream: false,
         messages: [
           {
-            role: "system",
-            content: "You are quite dramatic"
+        role: "system",
+        content: "You are quite dramatic"
           },
           {
-            role: "user",
-            content: `Analyze the mood of this diary entry: ${entries.map(entry => entry.content).join(' ')}`
+        role: "user",
+        content: `Analyze the mood of this diary entry: ${entries[0].content}`
           }
         ]
       });
@@ -49,14 +47,10 @@ const MoodAIAnalysis = ({ entries }) => {
       const response = await fetch('https://gen-ai-wbs-consumer-api.onrender.com/api/v1/chat/completions', requestOptions);
       if (!response.ok) {
         const errorText = await response.text();
-        console.error('Error Response Text:', errorText);
         throw new Error(`Failed to fetch AI analysis: ${errorText}`);
       }
       const data = await response.json();
       data.message.content = data.message.content.replace(/[\n\/]/g, ''); // Remove new lines and slashes
-
-
-      console.log('AI Response Data:', data);
 
       // Extract and set only the content from the response
       setAiSummary(data.message.content);
@@ -67,8 +61,6 @@ const MoodAIAnalysis = ({ entries }) => {
       setLoading(false); // Stop loading state
     }
   };
-
-  // Example chart data for Chart.js
   
 
   const chartData = aiSummary && typeof aiSummary === 'string' ? {
@@ -89,8 +81,6 @@ const MoodAIAnalysis = ({ entries }) => {
       borderWidth: 1,
     }],
   } : null;
-
-  console.log('Chart Data:', chartData); // Log the chart data
 
   return (
     <>
